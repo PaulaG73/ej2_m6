cec150d8a3895d0a8be9a105185992fb76433948
Paula Gajardo <paulagajardosch@gmail.com>
2026-02-27 16:25:17 -0300

    Subiendo Proyecto preliminar

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+ 
+ <h1>Fundación "Mi Casa"</h1>
+ <h3>Ficha de Mascotas</h3>
+ <nav class="nav-tabs">
+<button v-for="comp, index in componentes" :key="index"
+@click= "componenteActivo = comp.nombre"
+:class= "['nav-btn', {activo: componenteActivo== comp.nombre}]"
+>
+{{ comp.label }}
+</button>
+
+ </nav>
+ <div class="contenido">
+ <FichaMascotas
+ v-if="componenteActivo== `FichaMascotas`"/>
+ 
+ </div>
+
+</template>
+
+<script setup>
+
+import { ref } from 'vue';
+import FichaMascotas from './components/FichaMascotas.vue';
+
+const componenteActivo = ref ("FichaMascotas")
+const componentes = [
+  {nombre: "FichaMascotas", label: "Listar Mascota"}]
+
+  
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/FichaMascotas.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="mascota">
+        <ol>
+            <li v-for="mascota in data" :key="mascota.id">
+                Nombre:{{ mascota.nombre }}
+                Tipo:{{ mascota.tipo }}
+                Edad:{{ mascota.edad }}
+                Estado de vacuna:{{ mascota.vacunado }}
+                <ul>
+                    <li v-for="caracteristica, index in mascota.caracteristicas" :key="index">
+                        {{ caracteristica }}
+                    </li>
+
+                </ul>
+            </li>
+        </ol>
+    </div>
+    
+
+    <table>
+        <thead>
+            <tr>
+                <th>Nombre</th>
+                <th>Tipo</th>
+                <th>Edad</th>
+                <th>Vacunas</th>
+                <th>Caracteristicas</th>
+
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="mascota in data" :key="mascota.id">
+                <td>{{ mascota.nombre }}</td>
+                <td>{{ mascota.tipo }}</td>
+                <td>{{ mascota.edad }}</td>
+                <td>{{ mascota.vacunado === true ? "Sí" : "No" }}</td>
+                <td>{{ mascota.caracteristicas }}</td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script setup>
+
+const data = [
+    { id: 1, nombre: "Pluma", tipo: "gato", edad: 10, vacunado: true, foto: "./assets/img/Leona.jpg", caracteristicas: ["maternal", "exótica", "conversadora"] },
+    { id: 2, nombre: "Pelusa", tipo: "gato", edad: 9, vacunado: true, foto: "./assets/img/Leona.jpg", caracteristicas: ["maternal", "exótica", "conversadora"] },
+    { id: 3, nombre: "Polvo", tipo: "gato", edad: 8, vacunado: true, foto: "./assets/img/Leona.jpg", caracteristicas: ["maternal", "exótica", "conversadora"] },
+    { id: 4, nombre: "Manchita", tipo: "gato", edad: 8, vacunado: true, foto: "./assets/img/Leona.jpg", caracteristicas: ["maternal", "exótica", "conversadora"] },
+    { id: 5, nombre: "Nieve", tipo: "gato", edad: 8, vacunado: true, foto: "./assets/img/Leona.jpg", caracteristicas: ["maternal", "exótica", "conversadora"] },
+    { id: 6, nombre: "Dalí", tipo: "gato", edad: 7, vacunado: true, foto: "./assets/img/Leona.jpg", caracteristicas: ["maternal", "exótica", "conversadora"] },
+    { id: 7, nombre: "Gala", tipo: "gato", edad: 7, vacunado: true, foto: "./assets/img/Leona.jpg", caracteristicas: ["maternal", "exótica", "conversadora"] },
+    { id: 8, nombre: "Nala", tipo: "gato", edad: 6, vacunado: true, foto: "./assets/img/Leona.jpg", caracteristicas: ["maternal", "exótica", "conversadora"] },
+    { id: 9, nombre: "Leona", tipo: "gato", edad: 4, vacunado: true, foto: "./assets/img/Leona.jpg", caracteristicas: ["maternal", "exótica", "conversadora"] },
+    { id: 10, nombre: "Mía", tipo: "gato", edad: 3, vacunado: true, foto: "./assets/img/Leona.jpg", caracteristicas: ["maternal", "exótica", "conversadora"] },
+    { id: 11, nombre: "Tom", tipo: "gato", edad: 2, vacunado: true, foto: "./assets/img/Leona.jpg", caracteristicas: ["maternal", "exótica", "conversadora"] },
+    { id: 12, nombre: "Opa", tipo: "perro", edad: 10, vacunado: false, foto: "./assets/img/Leona.jpg", caracteristicas: ["maternal", "exótica", "conversadora"] }
+
+]
+
+</script>
+
+<style scoped></style>
